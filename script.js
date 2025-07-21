231b7922341070b4b5de5b9c9f5bd6e75d8f142a
ctrlARLTdel <148817830+ctrlARLTdel@users.noreply.github.com>
2025-07-21 16:04:46 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function testFunction() {
+    alert('Test successful! Your local browser app is working.');
+    console.log('Browser app test completed');
+}
+
+// Add any additional JavaScript functionality here
+console.log('Browser app loaded successfully');
